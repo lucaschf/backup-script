3a8e77cc8048627b364a185e4cdb86bf963b9149
Lucas Cristovam <lucaschfonseca@gmail.com>
2022-01-12 16:31:04 -0300

    task: translate messages to english

--- script-LUCAS.sh
+++ script-LUCAS.sh
@@ -5,7 +5,7 @@ declare -r config_file_path=/etc/backup-LUCAS/backup.conf
 
 
 if [[ ! -f $config_file_path ]]; then
-    echo "ERR: Arquivo de configuração não encontrado"
+    echo "ERR: Config file not found."
     exit 1
 fi   
 
@@ -22,22 +22,22 @@ set -euo pipefail
 
 function check-config {
     if [ -z ${logdir+x} ]; then 
-        echo-err "O diretório de logs(logdir) não foi definido em '$config_file_path'."
+        echo-err "The log directory(logdir) has not been set in '$config_file_path'."
         exit 1
     fi
 
     if [ -z ${logfile+x} ]; then 
-        echo-err "O arquivo de logs(logfile) não foi definido em '$config_file_path'."
+        echo-err "The log file(logfile) has not been set in '$config_file_path'."
         exit 1
     fi
 
     if [ -z ${targetdirs+x} ]; then 
-        echo-err "Os diretórios para backup(targetdirs) não foram definidos em '$config_file_path'."
+        echo-err "The directories for backup(targetdirs) has not been set in '$config_file_path'."
         exit 1
     fi
     
     if [ -z ${backupdir+x} ]; then 
-        echo-err "O diretório de armazenamento de backups(backupdir) não foi definido em '$config_file_path'."
+        echo-err "The backup storage directory(backupdir) has not been set in  '$config_file_path'."
         exit 1
     fi
 }
@@ -52,63 +52,68 @@ function echo-info {
 
 function check-and-create-directories-and-files-as-needed {
     if [[ ! -e $logdir ]]; then
-        echo-info "Criando diretório de logs..."
+        echo-info "Creating the logs directory..."
 
         if ! mkdir -p $logdir; then
-            echo-err "Abortando...."
+            echo-err "Aborting...."
             exit 1
         fi
 
-        echo-info "Diretório de logs criado"
+        echo-info "Logs directory created successfully."
     fi
 
     if [[ ! -e $log_path ]]; then
-        echo-info "Criando arquivo de logs..."
+        echo-info "Creating the logs file..."
 
         if ! touch "$log_path"; then
-            echo-err "Abortando..."
+            echo-err "Aborting..."
             exit 1
         fi
 
-        echo-info "Arquivo de logs criado"
+        echo-info "Logs file created successfully."
     fi  
 
     if [[ ! -e $backupdir ]]; then
-        echo-info "Criando diretório de backups..."
+        echo-info "Creating the backup storage directory..."
 
         if ! mkdir -p $backupdir; then
-            echo-err "Abortando...."
+            echo-err "Aborting..."
             exit 1
         fi
 
-        echo-info "Diretório de backups criado"
+        echo-info "backup storage directory created successfully."
     fi
 }
 
 # Verifica se os utilitarios necessarios para a execucao estao instalados
 function check-tools {
     which tar >/dev/null || {
-        echo-err "utilitário tar não foi encontrado"
+        echo-err "tar utility not found."
+        echo-err "Aborting..."
         return 1
     }
 
     which sha256sum >/dev/null || {
-        echo-err "utilitário sha256sum não foi encontrado"
+        echo-err "sha256sum utility not found."
+        echo-err "Aborting..."
         return 1
     }
 
     which bzip2  >/dev/null || {
-        echo-err "utilitário bzip2 não foi encontrado"
+        echo-err "bzip2 utility not found."
+        echo-err "Aborting..."        
         return 1
     }
 }
 
 function do-verify-backup-integrity {
-    declare backup_name="${1}"       
+    declare backup_file="${1}"       
+
+    declare -r backup_name="${backup_file##*/}"
     declare -r stored_hash_info=$(grep "$backup_name:" $log_path)
 
     if [[ ! $stored_hash_info ]]; then
-        echo-err "Registro de backup '$backup_name' não encontrado."
+        echo-err "Backup record '$backup_name' not found."
         exit 1
     fi
    
@@ -116,16 +121,16 @@ function do-verify-backup-integrity {
     read -ra arr <<< "$stored_hash_info"
     declare stored_hash=${arr[-1]}
     
-    backup_name=$(echo $backup_name | xargs)
-    declare backup_file="$backupdir/$backup_name"
+    # backup_name=$(echo $backup_name | xargs)
+    # declare backup_file="$backupdir/$backup_name"
     
     if ! is-backup-file-in-accepted-format $backup_file; then
-        echo-err "Arquivo de backup inválido."
+        echo-err "Invalid backup file."
         exit 1
     fi
 
     if [[ ! -f $backup_file ]]; then
-        echo-err "Arquivo de backup nao encontrado"
+        echo-err "Backup record '$backup_name' not found."
         exit 1
     fi
 
@@ -135,9 +140,9 @@ function do-verify-backup-integrity {
     echo-info "File calculated hash: $current_hash"
 
     if [[ $stored_hash == $current_hash ]]; then
-        echo-info "Backup íntegro."
+        echo-info "Backup íntegrity is intact."
     else
-        echo-info "Integridade do backup comprometida."
+        echo-info "Backup íntegrity compromised."
     fi
 
     return 0
@@ -145,16 +150,16 @@ function do-verify-backup-integrity {
 
 function do-show-usage {
     cat >&2 <<EOF
-Usage: ${exe_path} <-h|-c <caminho para o arquivo bz2>|-r <caminho para o arquivo bz2> [diretorios para restaurar...]|-b>
+Usage: ${exe_path} <-h|-c <path to the bz2 file>|-r <backup_file_path> [directories to restore...]|-b>
 
-    -c <caminho>                   : Verifica o sha256 do arquivo <caminho> comparando-o com o log de execução
+    -c <path>                      : Checks the sha256 of the <path> file by comparing it with the execution log
 
-    -r <backup_file_path> [dir...] : Restaura o conteúdo a partir do arquivo de backup <backup_file_path>. 
-                                   O parametro dir é opcional e caso informado, apenas os diretorios especificados serão restaurados.
+    -r <backup_file_path> [dir...] : Restores content from the backup file in <backup_file_path>. If the parameter dir is informed, 
+                                     only the specified directories are restored, otherwise a full restore will be performed
 
-    -b                             : Efetua o backup de acordo com o arquivo .conf
+    -b                             : performs the backup according to the .conf file
 
-    -h                             : Exibe este menu
+    -h                             : display this help menu
 
 EOF
 }
@@ -169,16 +174,16 @@ function do-create-backup {
     declare backup_name="backup-$(date +"%Y%m%d")-$(date +"%H%M").tar.bz2" 
     declare backup_path="$backupdir/$backup_name"    
 
-    echo-info "Realizando backup...."
+    echo-info "Performing backup. Please be patient..."
 
     archive-directiories $backup_path
     backup_end_time=$(date +"%H:%M:%S")
     
     readonly backup_end_time
 
-    echo-info "Salvando logs de backup..."
+    echo-info "Generating logs..."
     save-log $log_path $backup_path
-    echo-info "Operação finalizada."
+    echo-info "Finished."
 }
 
 # verifica se os diretorios especificados para backup sao diretorios validos.
@@ -189,7 +194,7 @@ function check-targetdirs {
     for path in "${directories[@]}"
      do  
         if [[ ! -d $path ]]; then
-            echo-err "O caminho '$path' não é válido. Abortando...."
+            echo-err "Invalid path'$path'. Aborting...."
             exit 1
         fi
      done
@@ -207,6 +212,8 @@ function archive-directiories {
 
 function calculate-hash {
     declare -r file="${1}"
+
+    echo-info "Calculating hash. Please be patient..."
     declare -r hash=$(sha256sum "${backup_file}" | cut -f 1 -d ' ')
 
     echo "$hash"
@@ -253,12 +260,12 @@ function do-backup-restore {
     declare target=""
 
     if ! is-backup-file-in-accepted-format $backup_file; then
-        echo-err "Arquivo de backup inválido."
+        echo-err "Invalid backup file."
         exit 1
     fi
 
     if [[ ! -f $backup_file ]]; then
-        echo-err "Arquivo de backup nao encontrado."
+        echo-err "Backup file not found."
         exit 1
     fi
 
@@ -268,16 +275,16 @@ function do-backup-restore {
         arg=${args[$i]}
 
         if ! tar -tf $backup_file $arg >/dev/null 2>&1; then
-            echo-err "O diretório '$arg' não está contido no backup '$backup_name'."
+            echo-err "'$arg' directory not found in backup file '$backup_name'."
             exit 1    
         fi
 
         target+=" $arg"
     done
 
-    echo-info "Iniciando restauração..."
+    echo-info "Performing restoration. Please be patient..."
     tar -C / -xf $backup_file $target
-    echo-info "Operação finalizada."
+    echo-info "Finished."
 }
 
 function main {
@@ -308,7 +315,6 @@ function main {
         esac
     done
     if [ $OPTIND -eq 1 ]; then
-        echo-err "No options were passed."
         do-show-usage && return 1
     fi
     shift $((OPTIND-1))
