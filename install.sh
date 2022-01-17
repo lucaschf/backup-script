293196863884b32365754c01811a7a3fde96f469
lucaschfonseca <lucaschfonseca@gmail.com>
2022-01-17 16:46:30 -0300

    task: added info about target dirs infoming

--- install.sh
+++ install.sh
@@ -67,7 +67,8 @@ logdir=$default_log_dir
 # log file name
 logfile=$default_log_file 
 
-# target directories for backup. For multiple directories use ',' to separate.
+# target directories for backup. For multiple directories use ',' to separate. For names containing whitespaces,
+use quotation marks. For example, the directory /home/user/some stuff should be entered as "/home/user/some stuff".
 targetdirs=$default_targetdirs
 
 # location to save created backups
